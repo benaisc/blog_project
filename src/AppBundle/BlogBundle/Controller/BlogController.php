89be0091c8b9cdefed1df481b9be410a4e2c6522
Louise <louise.fresquet@etu.umontpellier.fr>
2017-12-29 20:26:54 +0100

    ajout contact + service mail

--- src/AppBundle/BlogBundle/Controller/BlogController.php
+++ src/AppBundle/BlogBundle/Controller/BlogController.php
@@ -12,7 +12,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class BlogController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name = "index")
      */
     public function indexAction()
     {
@@ -27,6 +27,59 @@ class BlogController extends Controller
     {
         return $this->render('default/about.html.twig');
     }
+
+
+
+
+    /**
+     * @Route("/contact", name="contact_me")
+     */
+    public function contactAction(Request $request)
+    {
+        // Create the form according to the FormType created previously.
+        // And give the proper parameters
+        $form = $this->createForm('AppBundle\Form\ContactType');
+
+        if ($request->isMethod('POST')) {
+            // Refill the fields in case the form is not valid.
+            $form->handleRequest($request);
+
+            if($form->isValid()){
+                // Send mail
+                if($this->sendEmail($form->getData())){
+
+                    // Everything OK, redirect to wherever you want ! :
+
+                    return $this->redirectToRoute('index');
+                }else{
+                    // An error ocurred, handle
+                    var_dump("Errooooor :(");
+                }
+            }
+        }
+
+        return $this->render('default/contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+
+    private function sendEmail($data){
+
+
+        $message = (new \Swift_Message($data['subject']))
+            ->setFrom($data['email'])
+            ->setTo('loufr@net-c.com')
+            ->setBody($data['message'])
+
+        ;
+        return $this->get('mailer')->send($message);
+
+    }
+
+
+
+
 
     /**
      * @Route("/post/{post_id}", name="blog_post", requirements={"post_id": "\d+"})
